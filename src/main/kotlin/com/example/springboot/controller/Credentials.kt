60b058b95d1d227278ba9f37bb9d9675348c5be1
jhordycg <jhordycg@pm.me>
2021-03-13 08:37:52 -0500

    some tuning in CredentialKt

--- src/main/kotlin/com/example/springboot/controller/Credentials.kt
+++ src/main/kotlin/com/example/springboot/controller/Credentials.kt
@@ -1,5 +1,8 @@
 package com.example.springboot.controller
 
+import org.springframework.boot.json.BasicJsonParser
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,22 +10,25 @@ import org.springframework.web.bind.annotation.RestController
 class Credentials {
     @GetMapping("/public")
     fun public(): String {
-        return "public"
+        return "Hello World!"
     }
 
     @GetMapping("/authenticated")
     fun authenticated(): String {
-        return "authenticated"
+        val auth: Authentication = SecurityContextHolder.getContext().authentication
+        return "${auth.name} is now an authenticated user!"
     }
 
     @GetMapping("/role/user")
     fun roleUser(): String {
-        return "roleUser"
+        val auth: Authentication = SecurityContextHolder.getContext().authentication
+        return "${auth.name} has a USER role"
     }
 
     @GetMapping("/role/admin")
     fun roleAdmin(): String {
-        return "roleAdmin"
+        val auth: Authentication = SecurityContextHolder.getContext().authentication
+            return "${auth.name} has a ADMIN role"
     }
 
 }
